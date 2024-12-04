1d215d45003d96cfc4fe6f39f4d54ac183761f40
Blessing Nzemo <blessingnzemojph@gmail.com>
2024-12-04 09:08:17 +0100

    composants livewire

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -10,5 +10,16 @@
         <input type="text" wire:model.lazy.debounce.10ms="title">
 
         Le titre actuel est : {{$title}}
+
+        <div class="mb-4 mt-4">
+            <button class="btn" wire:click.prevent="addOption">Add Option</button>
+          </div>
+          <div>
+            @foreach ($options as $index => $option)
+              <div class="mb-4">
+                {{ $index }} - Option {{ $index }} {{$option}}
+              </div>
+            @endforeach
+          </div>
     </form>
 </div>
